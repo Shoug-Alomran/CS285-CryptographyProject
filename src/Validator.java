36482ebfbcca7e06c4870146fd08667b6b74a3b8
Shoug Alomran <Shoug.alomran@hotmail.com>
2025-10-26 12:24:05 +0300

    Minor formatting.

--- src/Validator.java
+++ src/Validator.java
@@ -5,8 +5,10 @@ public class Validator {
 
     // true if q is a prime number ≥ 3
     public static boolean isPrime(BigInteger q) {
-        if (q == null) return false;
-        if (q.compareTo(BigInteger.valueOf(3)) < 0) return false; // reject < 3
+        if (q == null)
+            return false;
+        if (q.compareTo(BigInteger.valueOf(3)) < 0)
+            return false; // reject < 3
         return q.isProbablePrime(100); // Miller–Rabin (built into BigInteger)
     }
 
